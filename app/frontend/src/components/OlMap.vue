29b56b2e3bf87924bff629b2bda739a1c2db4a19
Leo <47145928+wittrockscode@users.noreply.github.com>
2023-11-05 23:13:50 +0100

    remove default vue example, add OpenLayers map component (#36)

---
+++ app/frontend/src/components/OlMap.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+ol-map(
+  :loadTilesWhileAnimating="true"
+  :loadTilesWhileInteracting="true"
+  style="height:100%;width: 100%;"
+  ref="mapRef"
+)
+  ol-view(
+    ref="view"
+    :center="center"
+    :rotation="rotation"
+    :zoom="zoom"
+    :projection="projection"
+  )
+  ol-tile-layer
+    ol-source-osm
+</template>
+
+<script lang="ts">
+import { ref } from "vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    const center = ref([40, 40]);
+    const projection = ref("EPSG:4326");
+    const zoom = ref(8);
+    const rotation = ref(0);
+
+    return { center, projection, zoom, rotation };
+  },
+});
+</script>
+
+<style scoped>
+</style>
