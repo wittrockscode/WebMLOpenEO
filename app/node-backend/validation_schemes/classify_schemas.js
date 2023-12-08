33c053902ef097f4e570394ea1cd2789dac2eac3
TimCi <cirothtim@gmail.com>
2023-12-08 17:09:44 +0100

    add AOI_SCHEMA

--- app/node-backend/validation_schemes/classify_schemas.js
+++ app/node-backend/validation_schemes/classify_schemas.js
@@ -8,7 +8,8 @@ const MODEL_SCHEMA = JOI.string().custom(function(value)
     {
       throw new Error("'model' must be 'RandomForest'");
     }
-  }).required();
+  }
+).required();
 
 const TOI_SCHEMA = JOI.object(
   {
@@ -19,7 +20,33 @@ const TOI_SCHEMA = JOI.object(
   }
 ).required();
 
-const AOI_SCHEMA = JOI.object().required();
+const POINT_SCHEMA = JOI.array().items(
+  JOI.number().min(-90).max(90).required(),
+  JOI.number().min(-180).max(180).required()
+).length(2).required();
+
+const POLYGON_SCHEMA = JOI.array().max(1).items(
+  JOI.array().items(POINT_SCHEMA).required()
+).required();
+
+const GEOMETRY_SCHEMA = JOI.object(
+  {
+    coordinates: POLYGON_SCHEMA.required(),
+    type: JOI.string().custom(function(value)
+      {
+          if (value !== 'Polygon')
+          {
+              throw new Error("'type' must be 'Polygon'")
+          }
+      }).required(),
+  }
+).required();
+
+const AOI_SCHEMA = JOI.object(
+  {
+    geometry: GEOMETRY_SCHEMA.required()
+  }
+).required();
 
 const TRAINING_DATA_SCHEMA = JOI.object().required();
 
