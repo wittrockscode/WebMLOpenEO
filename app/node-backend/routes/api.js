092adbbb226bdcbbe94cbe6af3fa962abe807ea3
TimCi <cirothtim@gmail.com>
2023-12-14 17:36:42 +0100

    Endpoint für PreRelease + y&x Min/Max calc für AOI

--- app/node-backend/routes/api.js
+++ app/node-backend/routes/api.js
@@ -3,17 +3,6 @@
 const EXPRESS = require('express');
 const ROUTER = EXPRESS.Router();
 
-// OpenAPI is used for Dokumentation and first limited validation
-const path = require('path');
-const OpenApiValidator = require('express-openapi-validator');
-const openApiSpec = require('./../openapi.json');
-const apiSpecPath = path.join(__dirname, './../openapi.json');
-
-// JOI is used for precised validation
-const {CLASSIFY_SCHEMA, validate_input} = require("../validation_schemes/classify_schemas");
-
-// Checks if request fits to OpenAPI-specification
-ROUTER.use(OpenApiValidator.middleware({ apiSpec: apiSpecPath, validateResponses: true, }));
 
 // API calls generally do not want caching because the returned data may change
 ROUTER.use(function(req, res, next)
@@ -22,42 +11,38 @@ ROUTER.use(function(req, res, next)
   next();
 });
 
-// GET OpenAPI-Documentation about this API 
-ROUTER.get('/', function(req, res) 
-{
-  res.setHeader('Content-Type', 'application/json');
-  res.send(openApiSpec);
-});
-
 // Train Model and classify map
-ROUTER.post('/classify', function(req, res) 
+ROUTER.post('/preRelease', function(req, res) 
 {
   try 
   {
-    // request mit JOI validieren:
-    let validation_result = validate_input(req.body, CLASSIFY_SCHEMA);
-    console.log("valid result", validation_result.errorDetails);
-
-    if (validation_result.hasError)
+    //y = Lng, x = Lat
+    let yMax,yMin, xMax, xMin;
+    for(let i=0;i<req.body.geometry.coordinates[0].length();i++)
     {
-      res.status(422).json({errors: validation_result.errorDetails});
-    }
-    else
-    {
-      //request an R-backend weiterleiten und ggf. darauf anpassen
-      let dummyResult = {
-        "Model": "Modeldata",
-        "Model-Result": {
-          //classes, propabilities, ...
-        }
+      if (req.body.geometry.coordinates[0][i][0] < yMin)
+      {
+        yMin = req.body.geometry.coordinates[0][i][0];
       }
-      res.setHeader('Content-Type', 'application/json');
-      res.send(dummyResult);
+      if (req.body.geometry.coordinates[0][i][0] > yMax)
+      {
+        yMax = req.body.geometry.coordinates[0][i][0];
+      }
+      if (req.body.geometry.coordinates[0][i][1] < xMin)
+      {
+        xMin = req.body.geometry.coordinates[0][i][1];
+      }
+      if (req.body.geometry.coordinates[0][i][1] > xMax)
+      {
+        xMax = req.body.geometry.coordinates[0][i][1];
+      }
     }
+
+
   } 
   catch (error) 
   {
-    console.error('Fehler beim Abrufen der Stationen:', error)
+    console.error('Fehler beim Abrufen des Pre-Release-Beispiels:', error)
     res.status(500).json({ message: 'Interner Serverfehler' })
   }
 });
