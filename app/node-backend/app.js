3df40807b198c746d51fd5640e7bd7b31c7d52f9
TimCi <cirothtim@gmail.com>
2023-12-17 21:51:18 +0100

    add swagger-rendered openapi.json

--- app/node-backend/app.js
+++ app/node-backend/app.js
@@ -6,6 +6,7 @@ let logger = require('morgan');
 
 let indexRouter = require('./routes/index');
 let apiRouter = require('./routes/api');
+let swaggerRouter = require('./routes/swagger');
 
 let app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/api', apiRouter);
+app.use('/swagger', swaggerRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
