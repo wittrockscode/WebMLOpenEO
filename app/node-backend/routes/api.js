65327102e021b043f7e14863695b0e584451cacb
TimCi <cirothtim@gmail.com>
2023-12-20 20:23:57 +0100

    delete unnecessery codeline (swagger-ui in api.js)

--- app/node-backend/routes/api.js
+++ app/node-backend/routes/api.js
@@ -27,9 +27,6 @@ let testmodel = './model.rds';
 // Checks if request fits to OpenAPI-specification
 ROUTER.use(OpenApiValidator.middleware({ apiSpec: apiSpecPath, validateResponses: true, }));
 
-// Serve Swagger UI
-ROUTER.use('/swagger-ui', EXPRESS.static('node_modules/swagger-ui'));
-
 // API calls generally do not want caching because the returned data may change
 ROUTER.use(function(req, res, next)
 {
