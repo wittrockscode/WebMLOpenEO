ca91583b6f47a713ab561f4f2eeac2a754401c11
tciroth <timciroth@uni-muenster.de>
2023-12-20 11:30:23 +0100

    rds download for prerelease

--- app/node-backend/routes/api.js
+++ app/node-backend/routes/api.js
@@ -117,4 +117,86 @@ ROUTER.post('/preRelease', async function(req, res)
   }
 });
 
+ROUTER.post('/preRelease2', async function(req, res) 
+{
+  try 
+  {
+    console.log("hi")
+    let east, west, north, south;
+    east = req.body.geometry.coordinates[0][0][0];
+    west = east;
+    north = req.body.geometry.coordinates[0][0][1];
+    south = north;
+    for(let i=0;i<req.body.geometry.coordinates[0].length;i++)
+    {
+      if (req.body.geometry.coordinates[0][i][0] < west)
+      {
+        west = req.body.geometry.coordinates[0][i][0];
+      }
+      if (req.body.geometry.coordinates[0][i][0] > east)
+      {
+        east = req.body.geometry.coordinates[0][i][0];
+      }
+      if (req.body.geometry.coordinates[0][i][1] < south)
+      {
+        south = req.body.geometry.coordinates[0][i][1];
+      }
+      if (req.body.geometry.coordinates[0][i][1] > north)
+      {
+        north = req.body.geometry.coordinates[0][i][1];
+      }
+    }
+
+    try 
+    {
+      // build connection
+      let client = new Connection(process.env.OPENEOCUBES_URI ?? "http://localhost:8000");
+
+      // basic login with default params
+      await client.authenticateBasic("user", "password");
+
+      // build a user-defined process
+      let builder = await client.buildProcess();
+
+      // load the initial data collection and limit the amount of data loaded
+      const datacubeInit = builder.load_collection(
+        'sentinel-s2-l2a-cogs',
+        { west: west, south: south, east: east, north: north }, // Bspl: { west: 563080.6, south: 4483092.4, east: 609472, north: 4530135 },
+        3857, // Bspl: 32618,
+        ['2021-06-01', '2021-06-30'],
+        ["B02","B03","B04"]
+      );
+      //console.log(datacubeInit)
+
+      //const formats = client.listFileTypes();
+      //console.log(formats.data.output);
+
+      // Save result as GeoTiff
+      const result = builder.save_result(datacubeInit, "RDS");
+      
+      // Process and download data synchronously
+      const startTime = new Date();
+
+      const blob_res = await client.computeResult(result);
+
+      const endTime = new Date();
+      const timeTaken = endTime - startTime;
+      console.log('Duration of process:', timeTaken);
+      res.writeHead(200, {
+        'Content-Type': blob_res.type,
+      });
+
+      blob_res.data.pipe(res);
+    } catch (error) {
+        console.error('Error:', error);
+    }
+
+  } 
+  catch (error) 
+  {
+    console.error('Fehler beim Abrufen des Pre-Release-Beispiels:', error)
+    res.status(500).json({ message: 'Interner Serverfehler' })
+  }
+});
+
 module.exports = ROUTER;
