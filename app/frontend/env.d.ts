277ec1765687c2b58b7248a6ef9e9befee1576fd
Leo <l.wittrock@ymail.com>
2024-01-07 15:51:50 +0100

    fix env types

--- app/frontend/env.d.ts
+++ app/frontend/env.d.ts
@@ -3,6 +3,8 @@
 interface ImportMetaEnv {
   readonly VITE_APP_TITLE: string
   readonly VITE_NODE_BACKEND_URI: string
+  readonly VITE_BASE_URL: string
+  readonly VITE_ENV: string
 }
 
 interface ImportMeta {
