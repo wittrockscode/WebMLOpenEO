43ef49d39a364d7f2bbf8e495180efe6fed25ba0
Martin Kriger <m.kriger@uni-muenster.de>
2024-01-14 14:49:25 +0100

    add classifytestfile

---
+++ app/node-backend/tests/classify.test.js
@@ -0,0 +1,19 @@
+const request = require('supertest');
+const app = require('../app');
+
+const classify_payload = require("../demodata.json");
+
+describe('API /api/classify endpoint test', () => {
+  test('GET /api/classify should accept correct data and send the model id', async () => {
+    const response = await request(app)
+      .post('/api/classify')
+      .send(classify_payload)
+      .set('Content-Type', 'application/json')
+      .set('Accept', 'application/json');
+
+    expect(response.statusCode).toBe(200);
+    expect(response.headers['content-type']).toEqual(expect.stringContaining('json'));
+    expect(response.body).toBeInstanceOf(Object);
+    expect(response.body).toHaveProperty('id');
+  });
+});
