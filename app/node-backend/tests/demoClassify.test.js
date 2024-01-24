2eff547f825ea283b13836488464ff8155e9a97e
TimCi <cirothtim@gmail.com>
2024-01-24 21:32:53 +0100

    add democlassify-test

---
+++ app/node-backend/tests/demoClassify.test.js
@@ -0,0 +1,21 @@
+const request = require('supertest');
+const app = require('../app');
+
+const classify_payload = require("../demodata.json");
+
+describe('API /api/demoClassify endpoint test', () => {
+  test('GET /api/demoClassify should accept correct data and send the model id', async () => {
+    const response = await request(app)
+      .post('/api/demoClassify')
+      .send(classify_payload)
+      .set('Content-Type', 'application/json')
+      .set('Accept', 'application/json');
+
+    expect(response.statusCode).toBe(200);
+    expect(response.headers['content-type']).toEqual(expect.stringContaining('json'));
+    expect(response.body).toBeInstanceOf(Object);
+    expect(response.body).toHaveProperty('model_id');
+    expect(response.body).toHaveProperty('classification');
+    expect(response.body).toHaveProperty('class_map');
+  }, 1000000);
+});
