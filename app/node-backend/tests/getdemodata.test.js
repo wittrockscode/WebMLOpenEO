5f6cfd2623ce9d80cd4d043f0f109489f907ed88
TimCi <cirothtim@gmail.com>
2024-01-24 21:32:33 +0100

    test getmodel: success-case

--- app/node-backend/tests/getdemodata.test.js
+++ app/node-backend/tests/getdemodata.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('./../app'); 
+const app = require('../app'); 
 
 describe('API getDemodata-Endpoint', () => {
   let server;
--- app/node-backend/tests/getmodel.test.js
+++ app/node-backend/tests/getmodel.test.js
@@ -1,7 +1,19 @@
 const request = require('supertest');
 const app = require('../app');
 
+const classify_payload = require("../demodata.json");
+
 describe('API /api/getModel endpoint test', () => {
+  let server;
+
+  beforeAll(() => {
+    server = app.listen(3000); // Start the server before tests
+  });
+  
+  afterAll(() => {
+    server.close(); // Shutdown the server after tests
+  });
+
   test('GET /api/getModel should return 404 on wrong id', async () => {
     const response = await request(app).get("/api/getModel?id=217751e2-b413-4fde-b88e-ab08b48ac7d3");
     expect(response.statusCode).toBe(404);
@@ -11,6 +23,19 @@ describe('API /api/getModel endpoint test', () => {
   });
 
   test("GET /api/getModel should return a .rds file on valid id", async () => {
-    //TODO
-  });
+    const response = await request(app)
+      .post('/api/demoClassify')
+      .send(classify_payload)
+      .set('Content-Type', 'application/json')
+      .set('Accept', 'application/json');
+    expect(response.statusCode).toBe(200);
+    expect(response.headers['content-type']).toEqual(expect.stringContaining('json'));
+    expect(response.body).toBeInstanceOf(Object);
+    expect(response.body).toHaveProperty('model_id');
+    expect(response.body).toHaveProperty('classification');
+    expect(response.body).toHaveProperty('class_map');
+
+    const model = await request(app).get("/api/getModel?id=" + response.body.model_id);
+    expect(model.statusCode).toBe(200);
+  }, 1000000);
 });
